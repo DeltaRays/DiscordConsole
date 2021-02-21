035feda761949c346612da044e65a5158cb2e401
DeltaRays <42403698+DeltaRays@users.noreply.github.com>
2021-02-21 12:59:01 +0100

    Implemented more gateway sections (the gateway section is almost ready to go)

--- src/main/kotlin/me/deltarays/discordconsole/DiscordSocket.kt
+++ src/main/kotlin/me/deltarays/discordconsole/DiscordSocket.kt
@@ -9,15 +9,23 @@ import org.java_websocket.client.WebSocketClient
 import org.java_websocket.handshake.ServerHandshake
 import java.net.URI
 import java.util.*
+import kotlin.math.ceil
 
 class DiscordSocket(uri: URI) : WebSocketClient(uri) {
-    lateinit var plugin: DiscordConsole
+    private lateinit var plugin: DiscordConsole
     lateinit var timer: Timer
     var lastS: String? = null
+    private var sessionId: String? = null
+    private var isConnected = false
+    private lateinit var botId: String
+    private var isInvalid = false
+
+    fun isConnected(): Boolean {
+        return this.isConnected
+    }
 
     companion object {
         fun getWSUrl(): String? {
-            val resp: String;
             val client = OkHttpClient()
             val request = try {
                 (Request.Builder()).url("https://discordapp.com/api/v8/gateway?v=8&encoding=json").build()
@@ -31,11 +39,11 @@ class DiscordSocket(uri: URI) : WebSocketClient(uri) {
             }
             val parser = JsonParser()
             val obj = try {
-                parser.parse(response.body().toString())
+                parser.parse(response.body()?.string())
             } catch (e: Exception) {
                 return null
             }
-            return obj.asJsonObject.get("url").toString()
+            return obj.asJsonObject.get("url").asString
         }
     }
 
@@ -45,57 +53,195 @@ class DiscordSocket(uri: URI) : WebSocketClient(uri) {
 
     override fun onOpen(handshakedata: ServerHandshake?) {
         if (plugin.config.getBoolean("debug", false))
-            plugin.logger.info("[WebSocket] Connected to discord!")
+            Utils.logColored(
+                plugin, "&a[Socket] Socket opened!", LogLevel.DEBUG
+            )
     }
 
-    val parser = JsonParser()
+    private val parser = JsonParser()
+    private var ackReceived = true
     override fun onMessage(message: String?) {
         val payload: JsonObject = try {
             parser.parse(message).asJsonObject
         } catch (e: Exception) {
             if (plugin.config.getBoolean("debug", false)) {
-                plugin.logger.error(("[WebSocket] Error!"))
+                Utils.logColored(
+                    plugin, "&4[WebSocket] Error!", LogLevel.SEVERE
+                )
                 e.printStackTrace()
-            } else
-                plugin.logger.error("[Discord Connection] Error!\nMessage: " + e.message)
+            } else Utils.logColored(
+                plugin,
+                "&c[Discord Connection] Error!\nMessage: &f" + e.message + "\n&cSet debug to true in the config to find out more!",
+                LogLevel.SEVERE
+            )
             return
         }
         val op: Int = payload.get("op").asInt
         val s = payload.get("s").asString
         if (s != null) this.lastS = s
         if (op == 10) {
-            val heartbeatInterval = payload.get("d").asJsonObject.get("heartbeat_interval").asLong
+            if (this.sessionId == null) {
+                val heartbeatInterval = payload.get("d").asJsonObject.get("heartbeat_interval").asLong
+                timer.schedule(object : TimerTask() {
+                    override fun run() {
+                        if (isInvalid) {
+                            timer.cancel()
+                            return
+                        }
+                        if (!ackReceived) {
+
+                            close(900, "No Heartbeat ACK received!")
+                        }
+                        val obj = JsonObject().apply {
+                            addProperty("op", 1)
+                            addProperty("d", lastS)
+                        }
+                        ackReceived = false
+                        send(obj.toString())
+                    }
+                }, 1, heartbeatInterval)
+                val obj = JsonObject().apply {
+                    addProperty("op", 2)
+                    add("d", JsonObject().apply {
+                        addProperty("token", plugin.config.getString("bot-token"))
+                        addProperty("intents", 1 shl 9)
+                        add("properties", JsonObject().apply {
+                            addProperty("\$os", "unknown")
+                            addProperty("\$browser", "DiscordSocket")
+                            addProperty("\$device", "DiscordConsole")
+                        })
+                    })
+                }
+                send(obj.toString())
+            } else {
+                val obj = JsonObject().apply {
+                    addProperty("op", 6)
+                    add("d", JsonObject().apply {
+                        addProperty("token", plugin.config.getString("bot-token"))
+                        addProperty("session_id", sessionId)
+                        addProperty("seq", lastS)
+                    })
+                }
+                send(obj.toString())
+            }
+        } else if (op == 0) {
+            when (payload.get("t").asString) {
+                "READY" -> handleReady(payload)
+                "RESUMED" -> handleResumed()
+                "MESSAGE_CREATE" -> handleMessage()
+            }
+        } else if (op == 9) {
+            Utils.logColored(
+                plugin,
+                "&e[Discord Connection] Unable to resume from when the bot was last connected! Creating a new connection...",
+                LogLevel.WARNING
+            )
+            this.sessionId = null
+            this.lastS = null
             timer.schedule(object : TimerTask() {
                 override fun run() {
-                    val obj = JsonObject().apply {
-                        addProperty("op", 1)
-                        addProperty("d", lastS)
-                    }
+                    reconnect()
                 }
-            }, 1, heartbeatInterval)
-
-            val obj = JsonObject().apply {
-                addProperty("op", 2)
-                add("d", JsonObject().apply {
-                    addProperty("token", "") // TODO("Add token")
-                    addProperty("intents", 1 shl 9)
-                    add("properties", JsonObject().apply {
-                        addProperty("\$os", "unknown")
-                        addProperty("\$browser", "DiscordSocket")
-                        addProperty("\$device", "DiscordConsole")
-                    })
-                })
-            }
-            this.send(obj.toString())
+            }, ceil(Math.random() * 5).toLong())
+        } else if (op == 7) {
+            this.close(900, "Discord asked me to reconnect")
         }
 
     }
 
-    override fun onClose(code: Int, reason: String?, remote: Boolean) {
-        TODO("Not yet implemented")
+    private fun handleReady(payload: JsonObject) {
+        isConnected = true
+        this.sessionId = payload.get("session_id").asString
+        this.botId = payload.get("user").asJsonObject.get("id").asString
+        Utils.logColored(plugin, "&a[Discord Connection] Successfully connected!", LogLevel.INFO)
     }
 
-    override fun onError(ex: Exception?) {
-        TODO("Not yet implemented")
+    private fun handleResumed() {
+        Utils.logColored(
+            plugin,
+            "&a[Discord Connection] Bot has successfully resumed from where it last disconnected!",
+            LogLevel.INFO
+        )
+    }
+
+    private fun handleMessage() {
+
+    }
+
+    override fun onClose(code: Int, reason: String?, remote: Boolean) {
+        this.isConnected = false
+        this.timer.cancel()
+        if (plugin.config.getBoolean("debug", false)) {
+            Utils.logColored(
+                plugin, String.format(
+                    "[WebSocket] Closed!\nRemote: %s\nCode: %s\nReason:%s",
+                    remote,
+                    code,
+                    reason
+                ), LogLevel.DEBUG
+            )
+        }
+        when (code) {
+            4004 -> {
+                Utils.logColored(
+                    plugin, "&cAn invalid bot token was provided!", LogLevel.SEVERE
+                )
+                this.isInvalid = true
+                return
+            }
+            4000 -> {
+                Utils.logColored(
+                    plugin,
+                    "&e[Discord Connection] Discord encountered an unknown error! Reconnecting...",
+                    LogLevel.WARNING
+                )
+                this.reconnect()
+            }
+            4001, 4002, 4003, 4005, 4007, 4012, 4013, 4014 -> {
+                Utils.logColored(
+                    plugin,
+                    "&4[Discord Connection] The plugin's author made an oopsie! Contact him on discord (https://discord.gg/WSaWztJ) and tell him that you encountered an issue with" +
+                            String.format(
+                                "code: %s and reason: %s", code, reason
+                            ),
+                    LogLevel.SEVERE
+                )
+                this.isInvalid = true
+            }
+            4008 -> {
+                Utils.logColored(
+                    plugin,
+                    "&e[Discord Connection] The bot was disconnected for doing something too fast! Reconnecting...",
+                    LogLevel.WARNING
+                )
+                this.reconnect()
+            }
+            4009 -> {
+                Utils.logColored(
+                    plugin, "&e[Discord Connection] The bot timed out! Reconnecting...", LogLevel.INFO
+                )
+                this.reconnect()
+            }
+            900 -> {
+                Utils.logColored(
+                    plugin, "&e[Discord Connection] The bot was disconnected! Reconnecting...", LogLevel.INFO
+                )
+                this.reconnect()
+            }
+        }
+
+    }
+
+    override fun onError(e: Exception?) {
+        if (plugin.config.getBoolean("debug", false)) {
+            Utils.logColored(
+                plugin, "&4[WebSocket] Error!", LogLevel.SEVERE
+            )
+            e?.printStackTrace()
+        } else Utils.logColored(
+            plugin,
+            "&c[Discord Connection] Error!\nMessage: &f" + e?.message + "\n&cSet debug to true in the config to find out more!",
+            LogLevel.SEVERE
+        )
     }
 }
