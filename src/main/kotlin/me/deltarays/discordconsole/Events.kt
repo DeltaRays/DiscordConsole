a15edaf6c226f4ea2eaac4a41dcd049d460c2a3e
DeltaRays <42403698+DeltaRays@users.noreply.github.com>
2021-02-21 12:59:35 +0100

    Added utils method to log coloured console messages, and added an Events class to find out when the server started up

---
+++ src/main/kotlin/me/deltarays/discordconsole/Events.kt
@@ -0,0 +1,12 @@
+package me.deltarays.discordconsole
+
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.server.ServerLoadEvent
+
+class Events(private val plugin: DiscordConsole) : Listener {
+    @EventHandler
+    public fun serverStartup(event: ServerLoadEvent) {
+        plugin.serverHasStartedUp = true;
+    }
+}
--- src/main/kotlin/me/deltarays/discordconsole/Utils.kt
+++ src/main/kotlin/me/deltarays/discordconsole/Utils.kt
@@ -1,9 +1,37 @@
 package me.deltarays.discordconsole
 
+import org.bukkit.Bukkit
 import org.bukkit.ChatColor
 
+/**
+ * An utils object, so that I don't need to keep repeating code
+ */
 object Utils {
+    /**
+     * Shorthand for translateAlternateColorCodes
+     * @param text The text in which to replace the color codes
+     * @return The text with the replaced color codes
+     */
     fun tacc(text: String): String {
         return ChatColor.translateAlternateColorCodes('&', text)
     }
+
+    fun logColored(plugin: DiscordConsole, text: String, level: LogLevel) {
+        val levelMsg =
+            when (level) {
+                LogLevel.WARNING -> "&e[WARNING] &r"
+                LogLevel.SEVERE -> "&4[SEVERE] &r"
+                LogLevel.DEBUG -> "&d[DEBUG] &r"
+                else -> ""
+            }
+        Bukkit.getConsoleSender()
+            .sendMessage(tacc(String.format("%s%s %s", levelMsg, plugin.getConfigManager().getPrefix(), text)))
+    }
+}
+
+enum class LogLevel {
+    INFO,
+    WARNING,
+    SEVERE,
+    DEBUG
 }
