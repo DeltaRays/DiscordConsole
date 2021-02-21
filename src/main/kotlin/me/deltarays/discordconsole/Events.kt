04be0b3154751717039d39a72e915d598eef9529
DeltaRays <42403698+DeltaRays@users.noreply.github.com>
2021-02-21 13:02:28 +0100

    Removed unnecessary arguments

--- src/main/kotlin/me/deltarays/discordconsole/Events.kt
+++ src/main/kotlin/me/deltarays/discordconsole/Events.kt
@@ -2,11 +2,10 @@ package me.deltarays.discordconsole
 
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
-import org.bukkit.event.server.ServerLoadEvent
 
 class Events(private val plugin: DiscordConsole) : Listener {
     @EventHandler
-    fun serverStartup(event: ServerLoadEvent) {
-        plugin.serverHasStartedUp = true;
+    fun serverStartup() {
+        plugin.serverHasStartedUp = true
     }
 }
