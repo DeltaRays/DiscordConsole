804bc8570e3690708aa575383040858772522283
DeltaRays <42403698+DeltaRays@users.noreply.github.com>
2021-02-21 12:57:29 +0100

    Added javadocs to CustomConfig.kt for readability

--- src/main/kotlin/me/deltarays/discordconsole/CustomConfig.kt
+++ src/main/kotlin/me/deltarays/discordconsole/CustomConfig.kt
@@ -1,22 +1,35 @@
 package me.deltarays.discordconsole
 
 import org.bukkit.configuration.file.YamlConfiguration
-import org.bukkit.configuration.file.YamlConstructor
-import org.yaml.snakeyaml.Yaml
 
 
+/**
+ * @author DeltaRays
+ * A custom configuration implementation
+ * Created to add comment support to snakeyaml
+ */
 class CustomConfig : YamlConfiguration() {
     var comments: Int = 0
-    private val yaml = Yaml(YamlConstructor())
+
+    /**
+     * Saves the yaml (with comments) to a string
+     * @return The yaml  string
+     */
     override fun saveToString(): String {
-        var str = super.saveToString()
-        var builder = StringBuilder()
+        val str = super.saveToString()
+        val builder = StringBuilder()
         str.split("\n").forEach { line ->
             builder.append(line.replaceFirst(Regex("cmt_\\d+!\\s*:"), "#") + "\n")
         }
         return builder.toString()
     }
 
+    /**
+     * Set a path to something and add comments before said path
+     * @param comments A list of comments
+     * @param path the path
+     * @param value The value at the path
+     */
     fun set(path: String, value: Any?, comments: Array<String>) {
         comments.forEach { comment ->
             super.set(String.format("cmt_%s!", this.comments), comment)
@@ -25,12 +38,22 @@ class CustomConfig : YamlConfiguration() {
         super.set(path, value)
     }
 
+    /**
+     * Set a path to something and add a comment before said path
+     * @param comment A comment
+     * @param path the path
+     * @param value The value at the path
+     */
     fun set(path: String, value: Any?, comment: String) {
         super.set(String.format("cmt_%s!", this.comments), comment)
         this.comments++
         super.set(path, value)
     }
 
+    /**
+     * @param contents The unparsed yaml
+     * Parses the comments of the yaml (by converting them to yaml objects)
+     */
     private fun parseComments(contents: String): String {
         val builder = StringBuilder()
         for (line in contents.split("\n")) {
@@ -50,6 +73,9 @@ class CustomConfig : YamlConfiguration() {
         return builder.toString()
     }
 
+    /**
+     * Loads the file from the unparsed yaml
+     */
     override fun loadFromString(contents: String) {
         super.loadFromString(parseComments(contents))
     }
