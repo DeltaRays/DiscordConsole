c1b9826be8a9315233d1340aa048a956c258bf51
DeltaRays <42403698+DeltaRays@users.noreply.github.com>
2021-02-21 12:57:00 +0100

    Created ConfigManager.kt to manage configuration stuff

---
+++ src/main/kotlin/me/deltarays/discordconsole/ConfigManager.kt
@@ -0,0 +1,26 @@
+package me.deltarays.discordconsole
+
+import java.io.File
+
+/**
+ * @author DeltaRays
+ * A class to manage the configuration file (for example to convert old config files to new versions without breaking anything)
+ */
+class ConfigManager(private val plugin: DiscordConsole) {
+    lateinit var configuration: CustomConfig
+    var configFile = File(plugin.dataFolder.absolutePath, "config.yml")
+
+    fun getPrefix(): String {
+        return configuration.getString("prefix", "&7[&6DiscordConsole&7]") as String
+    }
+
+    /**
+     * Loads the configuration file inside the {@see configuration}
+     */
+    fun loadConfig() {
+        if (!configFile.exists()) configFile.parentFile.mkdirs()
+        configFile.createNewFile()
+        configuration = CustomConfig()
+        configuration.load(configFile)
+    }
+}
