9bfbd31eb493da92f3f58c7fb7849f0e0bbb6ff6
DeltaRays <42403698+DeltaRays@users.noreply.github.com>
2021-02-21 12:58:16 +0100

    Moved configuration stuff from DiscordConsole.kt to ConfigManager.kt

--- src/main/kotlin/me/deltarays/discordconsole/DiscordConsole.kt
+++ src/main/kotlin/me/deltarays/discordconsole/DiscordConsole.kt
@@ -2,37 +2,52 @@ package me.deltarays.discordconsole
 
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
-import org.bukkit.configuration.file.YamlConfiguration
+import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
-import java.io.File
 import java.net.URI
 
+/**
+ * @author DeltaRays
+ * The main class of the DiscordConsole plugin
+ */
 class DiscordConsole : JavaPlugin() {
-    val logger: Logger = LogManager.getRootLogger();
-    private lateinit var configuration: CustomConfig
-    private var configFile = File(dataFolder.absolutePath, "config.yml")
-    fun loadConfig() {
-        if (!configFile.exists()) configFile.parentFile.mkdirs()
-        configFile.createNewFile()
-        configuration = CustomConfig()
-        configuration.load(configFile)
+    val logger: Logger = LogManager.getRootLogger()
+    private val configManager = ConfigManager(this)
+    var serverHasStartedUp = false;
+
+    /**
+     * Gets the custom yaml configuration
+     */
+    override fun getConfig(): CustomConfig {
+        return configManager.configuration
     }
 
-    override fun getConfig(): YamlConfiguration {
-        return configuration
+    fun getConfigManager(): ConfigManager {
+        return configManager
     }
 
-
+    override fun onLoad() {
+        configManager.loadConfig()
+        newSocket()
+        socket.connect()
+        Bukkit.getPluginManager().registerEvents(Events(this), this)
+    }
 
     override fun onEnable() {
     }
 
+
     private lateinit var socket: DiscordSocket
-    fun newSocket() {
+
+    /**
+     * Creates a new socket
+     * (Note, should only be called once as usually socket.reconnect() is used in case of disconnections.)
+     */
+    private fun newSocket() {
         val url = DiscordSocket.getWSUrl()
         if (url == null)
             logger.error("Error encountered while connecting to discord!")
-        socket = DiscordSocket(URI.create(url as String))
+        socket = DiscordSocket(URI.create(url))
         socket.setHandlingPlugin(this)
     }
 
